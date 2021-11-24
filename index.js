35bc06a7ee400707584432c7dfe96244820ef9fc
husniababaeva <husnia.babaeva@gmail.com>
2021-11-24 17:12:56 +0400

    some adds to pages

---
+++ index.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+  $(this).scrollTop(0);
+  var scroll_start = 0;
+  var startchange = $("#header");
+  var offset = startchange.offset();
+  if (startchange.length) {
+    $(document).scroll(function () {
+      scroll_start = $(this).scrollTop();
+      if (scroll_start > offset.top) {
+        $("#header").css({
+          "background-color": "rgba(255, 255, 255, 0.30)",
+          "backdrop-filter": "blur(4px)",
+        });
+      } else {
+        $("#header").css({ "background-color": "transparent" });
+      }
+    });
+  }
+
+  $(".hamburger").click(function () {
+    var $this = $(this);
+
+    if ($this.hasClass("is-active")) {
+      $(".fsmenu, .logo").removeClass("is-active");
+      $(".fsmenu, .logo").addClass("close-menu");
+    } else {
+      $(".fsmenu, .logo").removeClass("close-menu");
+      $(".fsmenu, .logo").addClass("is-active");
+    }
+    $this.toggleClass("is-active");
+  });
+
+  $(".fsmenu--list-element").hover(
+    function () {
+      $(this).addClass("open");
+      $(this).removeClass("is-closing");
+    },
+    function () {
+      $(this).removeClass("open");
+      $(this).addClass("is-closing");
+    }
+  );
+});
+var swiper = new Swiper(".autoslide", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
