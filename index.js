cc10e68091ab6e891944d8de63d159a7cb56ac42
husniababaeva <husnia.babaeva@gmail.com>
2021-12-02 15:22:38 +0400

    avanti some changes

--- index.js
+++ index.js
@@ -55,25 +55,25 @@ var swiper = new Swiper(".autoslide", {
 });
 
 //filter-portfolio
-$("#mix-wrapper").mixItUp({
-  load: {
-    sort: "order:asc",
-  },
-  animation: {
-    effects: "fade rotateZ(-180deg)",
-    duration: 700,
-  },
-  selectors: {
-    target: ".mix-target",
-    filter: ".filter-btn",
-    sort: ".sort-btn",
-  },
-  callbacks: {
-    onMixEnd: function (state) {
-      console.log(state);
-    },
-  },
-});
+// $("#mix-wrapper").mixItUp({
+//   load: {
+//     sort: "order:asc",
+//   },
+//   animation: {
+//     effects: "fade rotateZ(-180deg)",
+//     duration: 700,
+//   },
+//   selectors: {
+//     target: ".mix-target",
+//     filter: ".filter-btn",
+//     sort: ".sort-btn",
+//   },
+//   callbacks: {
+//     onMixEnd: function (state) {
+//       console.log(state);
+//     },
+//   },
+// });
 // $(function () {
 //   var filterList = {
 //     init: function () {
