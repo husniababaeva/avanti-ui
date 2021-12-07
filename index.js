2839a253b1d432737358ba697afe06fc04f2fdf9
husniababaeva <husnia.babaeva@gmail.com>
2021-12-07 17:53:58 +0400

    some adddds

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ $(document).ready(function () {
         $("#header").css({
           "background-color": "rgba(255, 255, 255, 0.30)",
           "backdrop-filter": "blur(4px)",
-          
         });
       } else {
         $("#header").css({ "background-color": "transparent" });
@@ -41,7 +40,58 @@ $(document).ready(function () {
       $(this).addClass("is-closing");
     }
   );
+
+  // Create object to store filter for each group
+  var buttonFilters = {};
+  var buttonFilter = "*";
+  // Create new object for the range filters and set default values
+  // The default values should correspond to the default values from the slider
+
+  // Initialise Isotope
+  // Set the item selector
+  var $grid = $(".grid").isotope({
+    itemSelector: ".item",
+    layout: "masonry",
+    // use filter function
+    filter: function () {
+      var $this = $(this);
+      return $this.is(buttonFilter);
+    },
+  });
+
+  // Look inside element with .filters class for any clicks on elements with .btn
+  $(".filters").on("click", ".btn", function () {
+    var $this = $(this);
+    // Get group key from parent btn-group (e.g. data-filter-group="color")
+    var $buttonGroup = $this.parents(".btn-group");
+    var filterGroup = $buttonGroup.attr("data-filter-group");
+    // set filter for group
+    buttonFilters[filterGroup] = $this.attr("data-filter");
+    // Combine filters or set the value to * if buttonFilters
+    buttonFilter = concatValues(buttonFilters) || "*";
+    // Log out current filter to check that it's working when clicked
+    console.log(buttonFilter);
+    // Trigger isotope again to refresh layout
+    $grid.isotope();
+  });
+
+  // change checked class on btn-filter to toggle which one is active
+  $(".btn-group").each(function (i, buttonGroup) {
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on("click", ".btn-filter", function () {
+      $buttonGroup.find(".checked").removeClass("checked");
+      $(this).addClass("checked");
+    });
+  });
 });
+function concatValues(obj) {
+  var value = "";
+  for (var prop in obj) {
+    value += obj[prop];
+  }
+  return value;
+}
+
 var swiper = new Swiper(".autoslide", {
   spaceBetween: 30,
   centeredSlides: true,
@@ -93,4 +143,3 @@ var swiper = new Swiper(".autoslide", {
 //   // Run the show!
 //   filterList.init();
 // });
-
